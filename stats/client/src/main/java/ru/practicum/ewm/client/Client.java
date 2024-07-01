b15ffd23454317b54503d2b14e978fc997595f8e
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-02 00:13:28 +0300

    feat: Реализовал клиент модуль

--- stats/client/src/main/java/ru/practicum/ewm/client/Client.java
+++ stats/client/src/main/java/ru/practicum/ewm/client/Client.java
@@ -1,14 +1,13 @@
-package ru.practicum.explorewithme.client;
+package ru.practicum.ewm.client;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import ru.practicum.ewm.dto.EndpointHit;
+import ru.practicum.ewm.dto.ViewStats;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,51 +15,42 @@ import java.util.List;
 @Slf4j
 public class Client {
     private final WebClient webClient;
-    private final String appName;
 
-    public Client(@Value("${stats-service.url}") String statsUrl,
-                  @Value("${spring.application.name}") String appName) {
+    public Client(@Value("${stats-service.url}") String statsUrl) {
         this.webClient = WebClient.create(statsUrl);
-        this.appName = appName;
     }
 
 
-//    public EndPointHit sendHit(HttpServletRequest request) {
-//        try {
-//            Mono<Void> monoResponse = webClient
-//                    .post()
-//                    .uri("/hit")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .body(endPointHit, EndpointHit.class)
-//                    .retrieve()
-//                    .bodyToMono(Void.class);
-//
-//            monoResponse.subscribe();
-//            Thread.sleep(5000);
-//        } catch (Exception ex) {
-//            log.error("error", ex);
-//        }
-//    }
+    public EndpointHit sendHit(EndpointHit endpointHit) {
+        return webClient
+                .post()
+                .uri("/hit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(endpointHit, EndpointHit.class)
+                .retrieve()
+                .bodyToMono(EndpointHit.class)
+                .doOnError(error -> log.error("Ошибка с клиентом /hit {}", error.getMessage()))
+                .block();
+    }
 
-    public void sendStats(HttpServletRequest request,
-                          LocalDateTime start,
+    public List<ViewStats> sendStats(LocalDateTime start,
                           LocalDateTime end,
                           List<String> uris,
                           Boolean unique) {
-        try {
-            Mono<String> monoResponse = webClient
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/stats/")
-                            .queryParam())
-                    .retrieve()
-                    .bodyToMono(String.class);
-            monoResponse.subscribe(s -> log.info("got response: " + s));
-            log.info("sleeping...");
-            Thread.sleep(5000);
-            log.info("completed");
-        } catch (Exception ex) {
-            log.error("error", ex);
-        }
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/stats/")
+                        .queryParam("start", start)
+                        .queryParam("end", end)
+                        .queryParam("uris", uris)
+                        .queryParam("unique", unique)
+                        .build())
+                .retrieve()
+                .bodyToMono(List.class)
+                .doOnError(error -> log.error("Ошибка с клиентом /stats/ {}", error.getMessage()))
+                .block();
+
     }
 }
+
