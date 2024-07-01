3e6d8a668b3ab4ef2618e997f71b23db1a6b7a80
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-02 02:16:02 +0300

    fix: убрал лишние импорты

--- stats/dto/src/main/java/ru/practicum/ewm/dto/EndpointHit.java
+++ stats/dto/src/main/java/ru/practicum/ewm/dto/EndpointHit.java
@@ -1,9 +1,7 @@
 package ru.practicum.ewm.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
