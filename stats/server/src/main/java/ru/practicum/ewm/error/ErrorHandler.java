6a9309c74f370b4c0b238b905b2505aca841278c
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-02 00:11:23 +0300

    feat: Добавил обработку ошибок

--- stats/server/src/main/java/ru/practicum/ewm/error/ErrorHandler.java
+++ stats/server/src/main/java/ru/practicum/ewm/error/ErrorHandler.java
@@ -1,2 +1,19 @@
-package ru.practicum.ewm.error;public class ErrorHandler {
+package ru.practicum.ewm.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.error("Произошла непредвиденная ошибка", e);
+        return new ErrorResponse("Произошла непредвиденная ошибка", e.getMessage(), ExceptionUtils.getStackTrace(e));
+    }
 }
