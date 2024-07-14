c795256e1133dc334fba18658b68202489739c66
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-14 04:05:07 +0300

    feat: Добавил конфиг файл

--- service/src/main/java/ru/practicum/ewm/service/config/Config.java
+++ service/src/main/java/ru/practicum/ewm/service/config/Config.java
@@ -1,2 +1,14 @@
-package ru.practicum.ewm.service.config;public class Config {
+package ru.practicum.ewm.service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.practicum.ewm.client.StatsClient;
+
+@Configuration
+public class Config {
+    @Bean
+    public static StatsClient statsClient(@Value("${stats-service.url}") String statsServiceUrl) {
+        return new StatsClient(statsServiceUrl);
+    }
 }
