cfada894bade20b6253e396b521f27fe4f16e63d
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-14 04:01:12 +0300

    fix: Добавил обработку ошибки с входными параметрами

--- stats/server/src/main/java/ru/practicum/ewm/server/error/ApiError.java
+++ stats/server/src/main/java/ru/practicum/ewm/server/error/ApiError.java
@@ -1,6 +1,8 @@
 package ru.practicum.ewm.server.error;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -9,13 +11,24 @@ public class ApiError {
     private String status;
     private final String message;
     private final String reason;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
     private String[] errors;
 
-    public ApiError(String reason, String message, String[] errors, String status, LocalDateTime timestamp) {
+    public ApiError(String reason, String message, String[] errors, String status,
+                    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime timestamp) {
         this.reason = reason;
         this.message = message;
         this.errors = errors;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public ApiError(String reason, String message, String status,
+                    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime timestamp) {
+        this.reason = reason;
+        this.message = message;
         this.status = status;
         this.timestamp = timestamp;
     }
--- stats/server/src/main/java/ru/practicum/ewm/server/error/ErrorHandler.java
+++ stats/server/src/main/java/ru/practicum/ewm/server/error/ErrorHandler.java
@@ -2,12 +2,13 @@ package ru.practicum.ewm.server.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.ewm.server.exception.DateValidationException;
 
 import java.time.LocalDateTime;
 
@@ -19,15 +20,23 @@ public class ErrorHandler {
     public ApiError handleValid(final MethodArgumentNotValidException e) {
         log.error("Ошибка с валидацией", e);
         return new ApiError(e.getMessage(), "Ошибка с валидацией",
-                ExceptionUtils.getStackFrames(e), HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now());
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), LocalDateTime.now());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleDataIntegrity(final DataIntegrityViolationException e) {
-        log.info("Нарушение целостности данных");
-        return new ApiError(e.getMessage(), "Нарушение целостности данных",
-                ExceptionUtils.getStackFrames(e), HttpStatus.CONFLICT.toString(), LocalDateTime.now());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleRequestParam(final MissingServletRequestParameterException e) {
+        log.error("Ошибка с валидацией параметров запроса", e);
+        return new ApiError(e.getMessage(), "Ошибка с валидацией параметров запроса",
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleDateValidation(final DateValidationException e) {
+        log.error("Ошибка с валидацией даты и времени", e);
+        return new ApiError(e.getMessage(), "Ошибка с валидацией даты и времени",
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), LocalDateTime.now());
     }
 
     @ExceptionHandler
@@ -35,6 +44,6 @@ public class ErrorHandler {
     public ApiError handleThrowable(final Exception e) {
         log.error("Произошла непредвиденная ошибка", e);
         return new ApiError(e.getMessage(), "Произошла непредвиденная ошибка",
-                ExceptionUtils.getStackFrames(e), HttpStatus.INTERNAL_SERVER_ERROR.toString(), LocalDateTime.now());
+                ExceptionUtils.getStackFrames(e), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), LocalDateTime.now());
     }
 }
