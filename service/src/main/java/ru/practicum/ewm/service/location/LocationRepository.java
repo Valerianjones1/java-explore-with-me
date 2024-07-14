5c31f5b0d2a31dc50891cd04d2da04db7d2bb2a0
Dinkiev Valery <dinkiev.valery@yandex.ru>
2024-07-14 04:02:55 +0300

    feat: Реализовал работу с локациями

--- service/src/main/java/ru/practicum/ewm/service/location/LocationRepository.java
+++ service/src/main/java/ru/practicum/ewm/service/location/LocationRepository.java
@@ -1,2 +1,10 @@
-package ru.practicum.ewm.service.location;public interface LocationRepository {
+package ru.practicum.ewm.service.location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByLatAndLon(double lat, double lon);
+
 }
--- service/src/main/java/ru/practicum/ewm/service/location/mapper/LocationMapper.java
+++ service/src/main/java/ru/practicum/ewm/service/location/mapper/LocationMapper.java
@@ -1,8 +1,23 @@
 package ru.practicum.ewm.service.location.mapper;
 
-import ru.practicum.ewm.service.location.dto.LocationDto;
+import ru.practicum.ewm.dto.location.LocationDto;
+import ru.practicum.ewm.service.location.Location;
 
 public class LocationMapper {
-    
-    public static LocationDto mapToLocation
+
+    public static Location mapToLocation(LocationDto locationDto) {
+        Location location = new Location();
+        location.setLat(locationDto.getLat());
+        location.setLon(locationDto.getLon());
+
+        return location;
+    }
+
+    public static LocationDto mapToLocationDto(Location location) {
+        LocationDto locationDto = new LocationDto();
+        locationDto.setLat(location.getLat());
+        locationDto.setLon(location.getLon());
+
+        return locationDto;
+    }
 }
